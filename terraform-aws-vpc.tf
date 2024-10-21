2a8beb8426c1cc26ac683c994b3c968c6337b6ce
Nagarjunyadavv <nagarjunyadav.vangu@hcltech.com>
2024-10-21 15:55:25 +0530

    changed region to Us east

--- terraform-aws-vpc.tf
+++ terraform-aws-vpc.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-1"  # Change to your desired region
+  region = "us-east-1"  # Change to your desired region
 }
 
 # Create VPC
@@ -15,7 +15,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-1a"  # Change as needed
+  availability_zone = "us-east-1a"  # Change as needed
 
   tags = {
     Name = "MainSubnet"
